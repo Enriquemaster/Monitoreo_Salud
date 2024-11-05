5d85e0b98ff1b6977a112dbac9cc1897a8306598
Enriquemaster <enriquemaster154@gmail.com>
2024-11-04 23:03:13 -0600

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,34 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import Globe from "@/components/ui/globe";
+import HyperText from "@/components/ui/hyper-text";
+import RetroGrid from "@/components/ui/retro-grid"; 
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding bg-gray-100 flex flex-col items-center justify-center h-auto w-full">
+      
+      
+      <HyperText
+      className="text-4xl font-bold text-black dark:text-blue"
+      text="Hyper Text"/>
+
+      
+{/* Texto adicional para probar Tailwind */}
+<h1 className="mt-4 text-4xl font-bold text-blue-500">
+          ¡Tailwind CSS está funcionando!
+        </h1>
+        <p className="mt-2 text-lg text-gray-700">
+          Este es un texto estilizado con Tailwind.
+        </p>
+
+
+   
+
       </IonContent>
     </IonPage>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,16 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
